5cc74e701b5ab3c129916b76be55794ea25d6a2a
luoqi <50949439+logeexpluoqi@users.noreply.github.com>
2024-09-20 08:51:42 +0000

    opt(qcli): execute string do not output to interface

--- qcli.h
+++ qcli.h
@@ -41,6 +41,7 @@ typedef struct {
     char *argv[QCLI_CMD_ARGC_MAX + 1];
     char history[QCLI_HISTORY_MAX + 1][QCLI_CMD_STR_MAX + 1];
     uint16_t args_size;
+    uint8_t is_exec_str;
     uint8_t args_index;
     uint8_t history_num;
     uint8_t history_index;
